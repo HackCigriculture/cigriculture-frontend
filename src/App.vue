f3758af43753d83d1a6d5dc3aa76bc6de3c4524e
Delton Ding <dsh0416@gmail.com>
2017-07-21 19:14:04 +0800

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+#app
+  md-toolbar
+    h2.md-title(style="flex: 1")
+      img.icon(src="/static/icon.png")
+      | Cigriculture
+  router-view
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="stylus" scoped>
+#app
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+.md-title
+  font-weight: bolder
+.icon
+  width: 32px
+  margin-right: 20px
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+#home
+  | Hello World
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="stylus">
+</style>
